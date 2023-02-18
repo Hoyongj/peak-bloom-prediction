19317a9c454707c79196e5e83e1a1a1d37f30647
hoyongj <hoyong1121@icloud.com>
2023-02-18 17:35:01 +0900

    worked on weather condition func

--- add_other_conditions.R
+++ add_other_conditions.R
@@ -1,21 +1,55 @@
 library(rnoaa)
+library(Hmisc)
 library(tidyverse)
 
+id_by_location <- function(location) {
+  if (location == "washingtondc") {
+    return("USC00186350")
+  }
+  if (location == "liestal") {
+    return("GME00127786")
+  }
+  if (location == "kyoto") {
+    return("JA000047759")
+  }
+  if (location == "vancouver") {
+    return("CA001108395")
+  }
+}
 
-a <- ghcnd_search(
-  stationid = "USC00186350",
+temperature_by_month <- function(location) {
+  ghcnd_search(
+    # stationid = id_by_location(location),
+    stationid = "JA000047759",
+    var = c("tmax"),
+    date_min = "1991-01-01",
+    date_max = "2023-01-31"
+  )[[1]] %>%
+    mutate(month = format(date, format = "%m")) %>%
+    mutate(year = format(date, format = "%Y")) %>%
+    group_by(year, id, month) %>%
+    summarize(tmax_avg = mean(tmax, na.rm = TRUE)) %>%
+    pivot_wider(
+      names_from = month,
+      names_prefix = "tmax_avg_",
+      values_from = tmax_avg
+    ) %>% impute() # some data like Sep. 2021 Japan has missing data 
+}
+
+c <- ghcnd_search(
+  # stationid = id_by_location(location),
+  stationid = "JA000047759",
   var = c("tmax"),
-  date_min = "2021-01-01",
+  date_min = "1991-01-01",
   date_max = "2023-01-31"
 )[[1]] %>%
   mutate(month = format(date, format = "%m")) %>%
-  group_by(id, month) %>%
+  mutate(year = format(date, format = "%Y")) %>%
+  group_by(year, id, month) %>%
   summarize(tmax_avg = mean(tmax, na.rm = TRUE)) %>%
-  pivot_wider(names_from = month, names_prefix = "tmax_avg_", values_from = tmax_avg)
+  pivot_wider(
+    names_from = month,
+    names_prefix = "tmax_avg_",
+    values_from = tmax_avg
+  )
 
-historic_temperatures <-tibble(
-  location = "washingtondc", get_temperature("USC00186350")) %>%
-  bind_rows(tibble(location = "liestal", get_temperature("GME00127786"))) %>%
-  bind_rows(tibble(location = "kyoto", get_temperature("JA000047759"))) %>%
-  bind_rows(tibble(location = "vancouver", get_temperature("CA001108395"))
-)
