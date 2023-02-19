dcbc288cd4cc7d146969b71e7be6a3fe4a6f0099
Hoyongj <hoyong1121@icloud.com>
2023-02-19 17:56:38 +0900

    made new column ready

--- add_other_conditions.R
+++ add_other_conditions.R
@@ -1,5 +1,4 @@
 library(rnoaa)
-library(Hmisc)
 library(tidyverse)
 
 id_by_location <- function(location) {
@@ -19,37 +18,26 @@ id_by_location <- function(location) {
 
 temperature_by_month <- function(location) {
   ghcnd_search(
-    # stationid = id_by_location(location),
-    stationid = "JA000047759",
+    stationid = id_by_location(location),
     var = c("tmax"),
     date_min = "1991-01-01",
-    date_max = "2023-01-31"
+    date_max = "2023-02-28"
   )[[1]] %>%
     mutate(month = format(date, format = "%m")) %>%
     mutate(year = format(date, format = "%Y")) %>%
-    group_by(year, id, month) %>%
+    group_by(year, month) %>%
     summarize(tmax_avg = mean(tmax, na.rm = TRUE)) %>%
     pivot_wider(
       names_from = month,
       names_prefix = "tmax_avg_",
       values_from = tmax_avg
-    ) %>% impute() # some data like Sep. 2021 Japan has missing data 
+    ) %>%
+    mutate(location = location) %>%
+    mutate(tmax_JanFeb = tmax_avg_01 + tmax_avg_02) %>%
+    select(location, year, tmax_JanFeb)
+    
 }
 
-c <- ghcnd_search(
-  # stationid = id_by_location(location),
-  stationid = "JA000047759",
-  var = c("tmax"),
-  date_min = "1991-01-01",
-  date_max = "2023-01-31"
-)[[1]] %>%
-  mutate(month = format(date, format = "%m")) %>%
-  mutate(year = format(date, format = "%Y")) %>%
-  group_by(year, id, month) %>%
-  summarize(tmax_avg = mean(tmax, na.rm = TRUE)) %>%
-  pivot_wider(
-    names_from = month,
-    names_prefix = "tmax_avg_",
-    values_from = tmax_avg
-  )
+c <- temperature_by_month("kyoto")
+
 
