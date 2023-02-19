11a9be0549e74a090558dacb88951743b7849f35
Hoyongj <hoyong1121@icloud.com>
2023-02-19 20:00:48 +0900

    clustered 4 diff months data

--- add_other_conditions.R
+++ add_other_conditions.R
@@ -1,6 +1,8 @@
 library(rnoaa)
 library(tidyverse)
 
+# Convert name to id
+
 id_by_location <- function(location) {
   if (location == "washingtondc") {
     return("USC00186350")
@@ -16,11 +18,13 @@ id_by_location <- function(location) {
   }
 }
 
-temperature_by_month <- function(location) {
-  ghcnd_search(
+# location name -> year, location, last 4 months' temperature
+
+temperature_last_4 <- function(location) {
+  tem_tb <- ghcnd_search(
     stationid = id_by_location(location),
     var = c("tmax"),
-    date_min = "1991-01-01",
+    date_min = "1990-11-01",
     date_max = "2023-02-28"
   )[[1]] %>%
     mutate(month = format(date, format = "%m")) %>%
@@ -29,15 +33,28 @@ temperature_by_month <- function(location) {
     summarize(tmax_avg = mean(tmax, na.rm = TRUE)) %>%
     pivot_wider(
       names_from = month,
-      names_prefix = "tmax_avg_",
+      names_prefix = "tmax_",
       values_from = tmax_avg
     ) %>%
     mutate(location = location) %>%
-    mutate(tmax_JanFeb = tmax_avg_01 + tmax_avg_02) %>%
-    select(location, year, tmax_JanFeb)
-    
+    select(location, year, tmax_01, tmax_02, tmax_11, tmax_12)
+  # mutate(tmax_last_4 = lag(tmax_12)) # lag not working
+  tem_tb$tmax_11_prev <-
+    c(NA, tem_tb$tmax_11[-length(tem_tb$tmax_11)]) # Alternative Implementation of `lag`
+  tem_tb$tmax_12_prev <-
+    c(NA, tem_tb$tmax_12[-length(tem_tb$tmax_12)])
+  tem_tb %>%
+    # select(-c(tmax_11,tmax_12)) %>%
+    mutate(tmax_last_4 = (tmax_11_prev + tmax_12_prev + tmax_01 + tmax_02) /
+             4) %>%
+    select(location,
+           year,
+           tmax_11_prev,
+           tmax_12_prev,
+           tmax_01,
+           tmax_02,
+           tmax_last_4)
 }
 
-c <- temperature_by_month("kyoto")
-
-
+# # temperature_last_4 demo
+# c <- temperature_last_4("kyoto")
